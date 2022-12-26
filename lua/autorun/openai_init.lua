572000cd00fab39313a6b261fe2a0ad42de9eca9
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-12-26 15:59:48 -0300

    Update openai_init.lua

--- lua/autorun/openai_init.lua
+++ lua/autorun/openai_init.lua
@@ -3,7 +3,13 @@ if SERVER then
     AddCSLuaFile("openai/cl_openai.lua")
     include("openai/sh_openai.lua")
     include("openai/sv_openai.lua")
+
+    openai.print("Loaded: openai/sh_openai.lua")
+    openai.print("Loaded: openai/sv_openai.lua")
 else
     include("openai/sh_openai.lua")
     include("openai/cl_openai.lua")
+
+    openai.print("Loaded: openai/sh_openai.lua")
+    openai.print("Loaded: openai/cl_openai.lua")
 end
